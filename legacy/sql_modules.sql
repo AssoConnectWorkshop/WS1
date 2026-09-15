573c23d3a412587a5d9a09ebc4f55f10a8a05e91
AssoConnectWorkshop <arnaud@assoconnect.com>
2026-09-15 22:24:25 +0200

    Add files via upload

---
+++ legacy/sql_modules.sql
@@ -0,0 +1,2 @@
+Msg 451, Level 16, State 1, Server 610732dbaba2, Line 1
+Cannot resolve collation conflict between "French_CI_AS" and "Latin1_General_CI_AS_KS_WS" in add operator occurring in SELECT statement column 1.
